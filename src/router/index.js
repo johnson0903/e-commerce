35d7590b8c7b2522b5b80f75f4056e5f47f07097
johnson0903 <johnson50701@gmail.com>
2020-10-30 14:49:53 +0800

    add router path to admin and login, helloworld component logout funtion

--- src/router/index.js
+++ src/router/index.js
@@ -1,22 +1,35 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+
+import Dashboard from '@/components/Dashboard'
+import Login from '@/components/pages/Login'
+import Products from '@/components/pages/Products'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
-    path: '/',
-    name: 'Home',
-    component: Home
+    path: '*',
+    redirect: 'login'
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/admin',
+    name: 'Dashboard',
+    component: Dashboard,
+    children: [
+      {
+        path: 'products',
+        name: 'Products',
+        component: Products,
+        meta: { requiresAuth: true }
+
+      }
+    ]
+  },
+  {
+    path: '/login',
+    name: 'Login',
+    component: Login
   }
 ]
 
