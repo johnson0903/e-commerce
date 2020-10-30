6cc910de1ead035ca090adb092663ad8d0026dc5
johnson0903 <johnson50701@gmail.com>
2020-10-30 14:45:09 +0800

    use axios, loading overlay, bootstrap, bus, navigation guard

--- src/main.js
+++ src/main.js
@@ -1,8 +1,36 @@
 import Vue from 'vue'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+import Loading from 'vue-loading-overlay'
+import 'vue-loading-overlay/dist/vue-loading.css'
+import 'bootstrap'
+
 import App from './App.vue'
 import router from './router'
+import './bus'
 
+Vue.use(VueAxios, axios)
 Vue.config.productionTip = false
+Vue.component('Loading', Loading)
+
+axios.defaults.withCredentials = true
+
+router.beforeEach((to, from, next) => {
+  if (to.meta.requiresAuth) {
+    const api = `${process.env.VUE_APP_API}/api/user/check`
+    axios.post(api).then(response => {
+      if (response.data.success) {
+        next()
+      } else {
+        next({
+          path: '/login'
+        })
+      }
+    })
+  } else {
+    next()
+  }
+})
 
 new Vue({
   router,
