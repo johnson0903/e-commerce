776f93b8c21ff4a0e92ff674ad92ffec00e9ceac
johnson0903 <johnson50701@gmail.com>
2020-10-30 22:34:01 +0800

    新增 side bar 分頁

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -4,65 +4,8 @@
     class="col-md-3 col-lg-2 d-md-block bg-light sidebar collapse"
   >
     <div class="sidebar-sticky pt-3">
-      <ul class="nav flex-column">
-        <li class="nav-item">
-          <a
-            class="nav-link active"
-            href="#"
-          >
-            <span data-feather="home" />
-            Dashboard <span class="sr-only">(current)</span>
-          </a>
-        </li>
-        <li class="nav-item">
-          <a
-            class="nav-link"
-            href="#"
-          >
-            <span data-feather="file" />
-            Orders
-          </a>
-        </li>
-        <li class="nav-item">
-          <a
-            class="nav-link"
-            href="#"
-          >
-            <span data-feather="shopping-cart" />
-            Products
-          </a>
-        </li>
-        <li class="nav-item">
-          <a
-            class="nav-link"
-            href="#"
-          >
-            <span data-feather="users" />
-            Customers
-          </a>
-        </li>
-        <li class="nav-item">
-          <a
-            class="nav-link"
-            href="#"
-          >
-            <span data-feather="bar-chart-2" />
-            Reports
-          </a>
-        </li>
-        <li class="nav-item">
-          <a
-            class="nav-link"
-            href="#"
-          >
-            <span data-feather="layers" />
-            Integrations
-          </a>
-        </li>
-      </ul>
-
       <h6 class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted">
-        <span>Saved reports</span>
+        <span>管理員</span>
         <a
           class="d-flex align-items-center text-muted"
           href="#"
@@ -73,40 +16,54 @@
       </h6>
       <ul class="nav flex-column mb-2">
         <li class="nav-item">
-          <a
+          <router-link
             class="nav-link"
-            href="#"
+            active-class="active"
+            to="/admin/products"
           >
-            <span data-feather="file-text" />
-            Current month
-          </a>
+            <i class="fas fa-box-open" />
+            產品列表
+          </router-link>
         </li>
         <li class="nav-item">
-          <a
+          <router-link
             class="nav-link"
-            href="#"
+            active-class="active"
+            to="/admin/orders"
           >
-            <span data-feather="file-text" />
-            Last quarter
-          </a>
+            <i class="fas fa-list-alt" />
+            訂單列表
+          </router-link>
         </li>
         <li class="nav-item">
-          <a
+          <router-link
             class="nav-link"
-            href="#"
+            active-class="active"
+            to="/admin/coupons"
           >
-            <span data-feather="file-text" />
-            Social engagement
-          </a>
+            <i class="fas fa-ticket-alt" />
+            優惠券
+          </router-link>
         </li>
+      </ul>
+      <h6 class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted">
+        <span>模擬功能</span>
+        <a
+          class="d-flex align-items-center text-muted"
+          href="#"
+          aria-label="Add a new report"
+        >
+          <span data-feather="plus-circle" />
+        </a>
+      </h6>
+      <ul class="nav flex-column mb-2">
         <li class="nav-item">
-          <a
+          <router-link
             class="nav-link"
-            href="#"
+            to="/customer_order"
           >
-            <span data-feather="file-text" />
-            Year-end sale
-          </a>
+            模擬訂單
+          </router-link>
         </li>
       </ul>
     </div>
